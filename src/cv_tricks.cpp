0a8babf760127baf1c219255b6d4338aec1c7bd2
Maksym Tatariants <userboxj@gmail.com>
2024-07-20 20:44:19 +0300

    add clang format file

--- src/cv_tricks.cpp
+++ src/cv_tricks.cpp
@@ -4,15 +4,12 @@
 #include <string>
 
 
-void cv_tricks_print_vector(const std::vector<std::string>& strings)
-{
-    for (const auto & string : strings)
-    {
-        spdlog::info("cv_tricks/0.1 {}", string);
-    }
+void cv_tricks_print_vector(const std::vector<std::string>& strings) {
+  for (const auto& string : strings) {
+    spdlog::info("cv_tricks/0.1 {}", string);
+  }
 }
 
-int main(int argc, char* argv[])
-{
-    cv_tricks_print_vector({"val1", "val2"});
+int main(int argc, char* argv[]) {
+  cv_tricks_print_vector({"val1", "val2"});
 }
